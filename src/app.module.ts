4ac8d543aef5a8128c4acc368bf77fcacd3a4b40
JomianTC <jomiantoca-2011@hotmail.com>
2025-04-26 10:38:18 -0600

    orders branch created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { UserModule } from './user/user.module';
 import { envs } from './config';
 import { TrucksModule } from './trucks/trucks.module';
 import { LocationsModule } from './locations/locations.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
 	imports: [
 		UserModule,
 		MongooseModule.forRoot(envs.mongoUrl),
 		TrucksModule,
-		LocationsModule
+		LocationsModule,
+		OrdersModule
 	],
 	controllers: [],
 	providers: [],
